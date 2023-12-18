8b02cd191d26119044962f5da0deea8b3a74ebf0
vasanth <maxdelz30@gmail.com>
2023-12-18 14:18:37 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,118 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+      "5G capable smartphone gives you an extra boost of speed so you can download a movie in seconds, enjoy smooth streaming in ultra clear HD, play games at home and on the go, and even share your 5G speed with friends",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "OS	Android 6.0 (Marshmallow), upgradable to Android 8.0 (Oreo), TouchWiz UI Chipset	Exynos 8890 Octa (14 nm) CPU	Octa-core (4x2.3 GHz Mongoose & 4x1.6 GHz Cortex-A53) GPU	Mali-T880 MP12",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 8,
+    company: "htc",
+    info:
+      "OS	Android 6.0.1 (Marshmallow), upgradable to Android 8.0 (Oreo), Sense UI Chipset	Qualcomm MSM8996 Snapdragon 820 (14 nm) CPU	Quad-core (2x2.15 GHz Kryo & 2x1.6 GHz Kryo) GPU	Adreno 530",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "OS	Android 6.0.1 (Marshmallow), upgradable to Android 8.0 (Oreo), Sense UI Chipset	Qualcomm MSM8996 Snapdragon 820 (14 nm) CPU	Quad-core (2x2.15 GHz Kryo & 2x1.6 GHz Kryo) GPU	Adreno 530",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "	OS	Android 12 Chipset	Qualcomm SM6375 Snapdragon 695 5G (6 nm) CPU	Octa-core (2x2.2 GHz Kryo 660 Gold & 6x1.7 GHz Kryo 660 Silver) GPU	Adreno 619.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "OS	iOS, upgradable to iOS 4.2.1 CPU	412 MHz ARM 11 GPU	PowerVR MBX.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 30,
+    company: "apple",
+    info:
+      "OS	iOS 10.0.1, upgradable to iOS 15.7 Chipset	Apple A10 Fusion (16 nm) CPU	Quad-core 2.34 GHz (2x Hurricane + 2x Zephyr) GPU	PowerVR Series7XT Plus (six-core graphics).",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 2,
+    company: "apple",
+    info:
+      ".",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+  
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
